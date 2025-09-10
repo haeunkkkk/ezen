bd256e303349293c990e23c92c6a238cfd9170e0
haeunkkkk <haeun990911@gmail.com>
2025-09-10 15:49:08 +0900

    ezen

---
+++ grid/asset/script.js
@@ -0,0 +1,39 @@
+//주석
+
+
+
+//변수
+// var, let, const
+// es5 자바스크립트 시절에는 var라는 걸 선언했었다 그런데 지금은 아니다
+
+// let a1 = 1;
+// let a2 = 2;
+
+// document.write(a1 + a2 + "<br>");
+
+// let a3 = 3;
+
+// document.write(a3 + "<br>");
+
+// document.write("문자열" + "<br>");
+
+
+
+// document.write(1 + "2" + "<br>");
+
+
+// document.write("문자열" + "<br>");
+
+let loginBtn = document.querySelector('.login');
+
+
+
+loginBtn.addEventListener("click", function (){
+    // alert("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
+
+    // prompt("아이디를 입력해주세요");
+
+    
+})
+
+
